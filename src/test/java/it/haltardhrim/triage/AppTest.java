26dba29884954aa88106ebb9f9100ad837754beb
haltardhrim <haltardhrim@gmail.com>
2022-12-13 11:56:09 +0100

    Aggiunte Assert per non dover più guardare il DB dopo i test.

--- src/test/java/it/haltardhrim/triage/AppTest.java
+++ src/test/java/it/haltardhrim/triage/AppTest.java
@@ -1,5 +1,6 @@
 package it.haltardhrim.triage;
 
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import org.junit.jupiter.api.*;
 
@@ -7,96 +8,127 @@ import org.junit.jupiter.api.*;
 public class AppTest {
 
 	static PazienteService pazServ;
-	ArrayList<Paziente> pazList;
+	static ArrayList<Paziente> pazList;
 	static Paziente paz;
 
 	@BeforeAll
 	// Inizializza servizio
 	static void beforeAll() {
-		System.out.println("---\n" + "Test 0: DeleteAllPazienti");
-
 		pazServ = new PazienteService();
 		paz = new Paziente();
-
 		pazServ.resetPazienti();
-		System.out.println("Tabella Pazienti ripulita.");
+
+		//Verifica: la tabella è vuota
+		pazList = pazServ.mostraCodaPazienti();
+		assertEquals(0,pazList.size(),"BeforeAll: tabella Pazienti non vuota");
 	}
 
 	@Test
 	// Test 1: Crea un nuovo Paziente e inseriscilo in tabella
 	void test1InsertPaziente() {
-    	System.out.println("---\n" + "Test 1: testInsertPaziente");
-
     	paz.setCodfisc("Pippo");
 		paz.setPrioritaIniz(paz.BIANCO);
 		pazServ.accogliPaziente(paz);
-		System.out.println("Paziente " + paz.getCodfisc() + " inserito con priorita " + paz.getPrioritaIniz() + ".");
          
 		paz.setCodfisc("Paperino");
 		paz.setPrioritaIniz(paz.GIALLO);
 		pazServ.accogliPaziente(paz);
-		System.out.println("Paziente " + paz.getCodfisc() + " inserito con priorita " + paz.getPrioritaIniz() + ".");
          
 		paz.setCodfisc("Pluto");
-		paz.setPrioritaIniz(paz.BIANCO);
+		paz.setPrioritaIniz(paz.ROSSO);
 		pazServ.accogliPaziente(paz);
-		System.out.println("Paziente " + paz.getCodfisc() + " inserito con priorita " + paz.getPrioritaIniz() + ".");
-	}
 
-	@Test
-	// Test 2: Mostra lista Pazienti in coda
-	void test2SelectPaziente() {
-    	System.out.println("---\n" + "Test 2: testSelectPaziente");
-
+		//Verifica: la tabella contiene i record inseriti
 		pazList = pazServ.mostraCodaPazienti();
-	}
 
-	@Test
-	// Test 3: Aggiorna Stato Paziente
-	void test3UpdatePazienteStato() {
-    	System.out.println("---\n" + "Test 3: testUpdatePazienteStato");
-
-		pazList = pazServ.mostraCodaPazienti();
 		for (Paziente p: pazList) {
-			if (p.getCodfisc().trim().equals("Paperino")) {
-				pazServ.operaPaziente(p);
-				System.out.println("Il paziente " + p.getCodfisc() + " è ora in stato " + p.getStato());
+			switch (p.getCodfisc().trim()) {
+				case "Pippo":
+					assertAll("Verifica Pippo",
+							() -> assertEquals("Pippo",p.getCodfisc().trim(),"Test 1: Pippo.codfisc errato"),
+							() -> assertEquals(p.BIANCO,p.getPrioritaIniz(),"Test 1: Pippo.prioritaIniz errata"));
+					break;
+				case "Paperino":
+					assertAll("Verifica Paperino",
+							() -> assertEquals("Paperino",p.getCodfisc().trim(),"Test 1: Paperino.codfisc errato"),
+							() -> assertEquals(p.GIALLO,p.getPrioritaIniz(),"Test 1: Paperino.prioritaIniz errata"));
+					break;
+				case "Pluto":
+					assertAll("Verifica Pluto",
+							() -> assertEquals("Pluto",p.getCodfisc().trim(),"Test 1: Pluto.codfisc errato"),
+							() -> assertEquals(p.ROSSO,p.getPrioritaIniz(),"Test 1: Pluto.prioritaIniz errata"));
+					break;
+				default:
+					fail("Test 1: Paziente errato: " + p.getCodfisc());
+					break;
 			}
 		}
 	}
 
 	@Test
-	// Test 4: Aggiorna Priorita Paziente
-	void test4UpdatePazientePriorita() {
-    	System.out.println("---\n" + "Test 4: testUpdatePazientePriorita");
+	// Test 2: Aggiorna Stato Paziente
+	void test2UpdatePazienteStato() {
+		pazList = pazServ.mostraCodaPazienti();
+		for (Paziente p: pazList) {
+			if (p.getCodfisc().trim().equals("Paperino")) {
+				pazServ.operaPaziente(p);
+			}
+		}
 
+		// Verifica: lo stato di Paperino è cambiato
+		pazList = pazServ.mostraCodaPazienti();
+
+		for (Paziente p: pazList) {
+			if (p.getCodfisc().trim().equals("Paperino")) {
+				assertEquals(p.IN_INTERVENTO,p.getStato(),"Test 2: Paperino.stato errato");
+			}
+		}
+	}
+
+	@Test
+	// Test 3: Aggiorna Priorita Paziente
+	void test3UpdatePazientePriorita() {
 		pazList = pazServ.mostraCodaPazienti();
 		for (Paziente p: pazList) {
 			if (p.getCodfisc().trim().equals("Pippo")) {
 				p.setPriorita(p.VERDE);
 				pazServ.aggravaPaziente(p);
-				System.out.println("Il paziente " + p.getCodfisc() + " ha ora priorità " + p.getPriorita());
+			}
+		}
+
+		// Verifica: la priorità di Pippo è cambiata
+		pazList = pazServ.mostraCodaPazienti();
+
+		for (Paziente p: pazList) {
+			if (p.getCodfisc().trim().equals("Pippo")) {
+				assertEquals(p.VERDE,p.getPriorita(),"Test 3: Pippo.priorita errata");
 			}
 		}
 	}
 
 	@Test
-    // Test 5: Elimina Paziente (che è diverso da segnarlo come Evaso! Viene cancellato del tutto)
-	void test5DeletePaziente() {
-    	System.out.println("---\n" + "Test 5: testDeletePaziente");
-
+    // Test 4: Elimina Paziente (che è diverso da segnarlo come Evaso! Viene cancellato del tutto)
+	void test4DeletePaziente() {
 		pazList = pazServ.mostraCodaPazienti();
-		for (Paziente paz: pazList) {
-			if (paz.getCodfisc().trim().equals("Pluto")) {
-				pazServ.eliminaPaziente(paz);
-				System.out.println("Il paziente " + paz.getCodfisc() + " è stato cancellato, dato che è un cane");
+		for (Paziente p: pazList) {
+			if (p.getCodfisc().trim().equals("Pluto")) {
+				pazServ.eliminaPaziente(p);
+			}
+		}
+
+		//Verifica: la tabella non contiene piu' Pluto
+		pazList = pazServ.mostraCodaPazienti();
+
+		for (Paziente p: pazList) {
+			if (p.getCodfisc().trim().equals("Pluto")) {
+				fail("Test 4: Pluto non cancellato");
 			}
 		}
 	}
 
-	@AfterEach
-	// Mostra il risultato
-	void afterEach() {
+	@AfterAll
+	// Mostra il risultato finale
+	static void afterAll() {
 		pazList = pazServ.mostraCodaPazienti();
 		System.out.println("Lista Coda Pazienti");
 		System.out.println("ID   Priorita   Cod.Fisc.");
